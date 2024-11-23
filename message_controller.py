09a17cbf12bb8ec9814d037bf5e7ce1cd7994eca
Rohit <rdas49@gatech.edu>
2024-11-23 12:40:00 -0800

    Add async operations to message controller

--- message_controller.py
+++ message_controller.py
@@ -23,7 +23,7 @@ class MessageController:
             raise ValueError("Target channel is not set.")
         await self.target_channel.send(msg)
 
-    def _send_help_message(self) -> None:
+    async def _send_help_message(self) -> None:
         """Send help message"""
         command_descriptions = {
             "!help": "print out all commands",
@@ -37,38 +37,39 @@ class MessageController:
         help_msg = "List of available commands: \n"
         for cmd, desc in command_descriptions.items():
             help_msg += f"\t{cmd} - {desc}\n"
-        self.send_message(help_msg)
+        await self.send_message(help_msg)
 
-    def _send_status_message(self) -> None:
+    async def _send_status_message(self) -> None:
         """Send status message of bot"""
         status_msg = f"Bot name: {self.name}\n"
         status_msg += "Tracked players:\n"
         for player, _ in self.players_to_track:
             status_msg += f"\t{player}\n"
 
-        self.send_message(status_msg)
+        await self.send_message(status_msg)
 
-    def _change_name(self, name) -> None:
+    async def _change_name(self, name) -> None:
         """Change activation name"""
         if self.name == name:
             return
         name_change_msg = f"Changed bot name from {self.name}"
         self.name = name
-        self.send_message(f"{name_change_msg} to {self.name}")
+        await self.send_message(f"{name_change_msg} to {self.name}")
 
-    def _show_registered_players(self) -> None:
+    async def _show_registered_players(self) -> None:
         registered_players_msg = "Registered players:\n"
         for player, tag in self.player_map:
             registered_players_msg += f"{player}: {tag}"
-        self.send_message(registered_players_msg)
+        await self.send_message(registered_players_msg)
 
-    def _add_player(self, player_name: str, player_tag: str):
+    async def _add_player(self, player_name: str, player_tag: str):
         if self.brawl_client.get_player_info(player_tag) is None:
-            return self.send_message(f"Unable to find player tag {player_tag}")
+            await self.send_message(f"Unable to find player tag {player_tag}")
+            return
         self.player_map[player_name] = player_tag
-        self.send_message(f"Added player {player_name} with player tag {player_tag}")
+        await self.send_message(f"Added player {player_name} with player tag {player_tag}")
 
-    def _start_tracking(self, names_to_track: str):
+    async def _start_tracking(self, names_to_track: str):
         """
         Start tracking a game. Log:
         - Trophy difference
@@ -79,14 +80,14 @@ class MessageController:
         names = names_to_track.split(" ")
         for name in names:
             if name not in self.player_map:
-                self.send_message(f"Player {name} is not registered")
+                await self.send_message(f"Player {name} is not registered")
                 return
             player_info = self.brawl_client.get_player_info(self.player_map[name])
             self.players_to_track[name] = player_info
             start_tracking_msg += "\tName: {name}, Start Trophies: {player_trophies}"
-        self.send_message(start_tracking_msg)
+        await self.send_message(start_tracking_msg)
 
-    def _end_tracking(self):
+    async def _end_tracking(self):
         """End tracking of players"""
         self.players_to_track = {}
         msg = "Ended Tracking\n"
@@ -101,7 +102,7 @@ class MessageController:
                 if end_trophies != start_trophies:
                     brawler_name = end_player_info["brawlers"][brawler_num]["name"]
                     msg += f"\t{brawler_name}: {end_trophies - start_trophies}"
-        self.send_message(msg)
+        await self.send_message(msg)
 
     def _validate_message(self, msg: str) -> bool:
         """Some basic validation on messages before analyzing"""
@@ -113,7 +114,7 @@ class MessageController:
             return False
         return True
 
-    def process_message(self, msg: str) -> None:  # noqa: C901 - allow complexity
+    async def process_message(self, msg: str) -> None:  # noqa: C901 - allow complexity
         """Process the message and perform action"""
         if not self._validate_message(msg):
             return
@@ -121,34 +122,36 @@ class MessageController:
 
         # One word commands
         if main_command == "!debug":
-            self.send_message(str(self))
-        if main_command == "!help":
-            return self._send_help_message()
-        if main_command == "!status":
-            return self._send_status_message()
+            await self.send_message(str(self))
+        elif main_command == "!help":
+            await self._send_help_message()
+        elif main_command == "!status":
+            await self._send_status_message()
 
-        if main_command == "!setname":
+        elif main_command == "!setname":
             full_command = msg.split(" ", 2)
             if len(full_command) != 3:
                 return
-            return self._change_name(full_command[-1])
+            await self._change_name(full_command[-1])
 
-        if f"!{self.name}" in msg:
+        elif f"!{self.name}" in msg:
             action = msg.split(f"!{self.name} ")[-1]
             if "start" in action:
                 if len(self.players_to_track) != 0:
-                    return self.send_message("Already tracking games")
-                return self._start_tracking(action.split("start ")[-1])
+                    await self.send_message("Already tracking games")
+                    return
+                await self._start_tracking(action.split("start ")[-1])
 
             elif "end" in action:
                 if len(self.players_to_track) == 0:
-                    return self.send_message("Currently not tracking any players")
-                return self._end_tracking()
+                    await self.send_message("Currently not tracking any players")
+                    return
+                await self._end_tracking()
 
             elif "add" in action:
                 name = action.split(" ", 2)[1]
                 tag = action.split(" ", 2)[-1]
-                self._add_player(name, tag)
+                await self._add_player(name, tag)
 
     def __str__(self) -> str:
         """Print all attributes of self"""
